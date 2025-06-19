06d8a8a45304628a7a732b2a2cf1575a4593a73e
Mac <macshah141@gmail.com>
2025-06-19 15:09:33 -0400

    Changes to the main.py file

--- 00_Onramp/onramp01_Cursor_and_Git/main.py
+++ 00_Onramp/onramp01_Cursor_and_Git/main.py
@@ -4,13 +4,13 @@ print("Hello, World!")
 #### ❓Question #1:
 # What is your name?
 print("What is your name?")
-name = # Add your name here
+name = 'Mac'
 print("Hello, " + name + "!")
 
 #### ❓Question #2:
 # Where do you live?
 print("Where do you live?")
-location = # Add your location here
+location = 'Toronto'
 print("I live in " + location + ".")
 
 
